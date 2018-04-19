cf63d59636ae4d3a94b713ce277adf56b49b2c3e
Thiago Gonzalez <contato@thiagogonzalez.com>
2018-04-19 20:26:51 -0300

    Responsive layout

--- assets/js/main.js
+++ assets/js/main.js
@@ -19788,30 +19788,32 @@ $(function() {
   });
 });
 
-var sliderContainer = $('#price-slider'),
-    slider = sliderContainer[0],
-    minValue = sliderContainer.data('min-value'),
-    maxValue = sliderContainer.data('max-value'),
-    maxText = $('#price-slider-max-text'),
-    minText = $('#price-slider-min-text');
+$(function() {
+  var sliderContainer = $('#price-slider'),
+      slider = sliderContainer[0],
+      minValue = sliderContainer.data('min-value'),
+      maxValue = sliderContainer.data('max-value'),
+      maxText = $('#price-slider-max-text'),
+      minText = $('#price-slider-min-text');
 
-noUiSlider.create(slider, {
-  start: [minValue, maxValue],
-  connect: true,
-  range: {
-    'min': minValue,
-    '25%': maxValue / 4,
-    '50%': maxValue / 2,
-    'max': maxValue
-  }
-});
+  noUiSlider.create(slider, {
+    start: [minValue, maxValue],
+    connect: true,
+    range: {
+      'min': minValue,
+      '25%': maxValue / 4,
+      '50%': maxValue / 2,
+      'max': maxValue
+    }
+  });
 
 
-slider.noUiSlider.on('update', function(values, handle) {
-  if (handle) {
-    maxText.text(Math.round(values[handle]));
-  } else {
-    minText.text(Math.round(values[handle]));
-  }
+  slider.noUiSlider.on('update', function(values, handle) {
+    if (handle) {
+      maxText.text(Math.round(values[handle]));
+    } else {
+      minText.text(Math.round(values[handle]));
+    }
+  });
 });
 
